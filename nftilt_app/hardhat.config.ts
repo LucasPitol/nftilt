9bb561ebdb26a644e00d2e42c53655db1fac49f3
Pitol <lucascpitol0@gmail.com>
2022-08-10 16:07:20 -0300

    dependências

--- nftilt_app/hardhat.config.ts
+++ nftilt_app/hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  paths: {
+    artifacts: './lib/artifacts',
+  }
 };
 
 export default config;
